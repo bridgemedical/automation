8273c7209d8b2794e3b05cbd475cb2f9d78bfb07
ah045577 <ah045577@WIN7DX64-02644.northamerica.cerner.net>
2017-04-26 19:19:21 +0530

    Systemmanager page object and functions modified

--- src/com/pageObject/Functions/SystemManagerBloodTypeChangeReasonPageFunctions.java
+++ src/com/pageObject/Functions/SystemManagerBloodTypeChangeReasonPageFunctions.java
@@ -1,23 +1,37 @@
 package com.pageObject.Functions;
 
 import org.openqa.selenium.support.PageFactory;
-
 import com.bridge.utilities.IEDriver;
 import com.pageObjects.SystemManagerBloodTypeChangeReasonPage;
 
-public class SystemManagerBloodTypeChangeReasonPageFunctions {
+
+/*
+ * Yet to implement explicit wait
+ * 
+*/
+
+
+
+public class SystemManagerBloodTypeChangeReasonPageFunctions 
+{
 
 	SystemManagerBloodTypeChangeReasonPage bloodtypechangepage = PageFactory.initElements(IEDriver.driver, SystemManagerBloodTypeChangeReasonPage.class );
-			
+	
 	//Method to create Reason for blood type change.
-	public void createReasonForBloodTypeChange(String code,String name,String description) throws InterruptedException
-	{
-		bloodtypechangepage.textboxCodeCreateReason().sendKeys(code);
+	public void createReasonForBloodTypeChange(String code,String name,String description) 
+	{		
+		try{
+		bloodtypechangepage.textboxCodeCreateReason.sendKeys(code);
 		Thread.sleep(2000);
-		bloodtypechangepage.textboxNameCreateReason().sendKeys(name);
+		bloodtypechangepage.textboxNameCreateReason.sendKeys(name);
 		Thread.sleep(2000);
-		bloodtypechangepage.textboxDescriptionCreateReason().sendKeys(description);
+		bloodtypechangepage.textboxDescriptionCreateReason.sendKeys(description);
 		Thread.sleep(2000);
-		bloodtypechangepage.buttonConfirmCreateReason().click();
+		bloodtypechangepage.buttonConfirmCreateReason.click();
+		}
+		catch(Exception e)
+		{
+			System.out.println("can't create reason for blood type change reason \n"+ e );
+		}
 	}	
 }
--- src/com/pageObjects/BabyMatchManagePatientInformationPage.java
+++ src/com/pageObjects/BabyMatchManagePatientInformationPage.java
@@ -8,29 +8,25 @@ import com.bridge.utilities.IEDriver;
 public class BabyMatchManagePatientInformationPage extends IEDriver
 {
 	
-	private static WebElement element = null;
+	
 	//Link to update mother details
-	@FindBy ( id = "editMother")
-	public WebElement editMotherLink;
+	@FindBy ( id = "editMother")			public WebElement editMotherLink;
+	
+	//Correct Mother Name field
+	@FindBy ( id = "txtCorrectMother_Name")	public WebElement correctMotherNameTextBox;
+	
+	//Correct Mother ID field
+	@FindBy ( id = "txtCorrectMother_ID")	public WebElement correctMotherIDTextBox;
+
+	//Confirm button
+	@FindBy ( id = "cmdOK")					public WebElement confirmButton;
+	
+	//Exit button
+	@FindBy ( id = "cmdCancel")				public WebElement exitButton;
 	
 	public WebElement getEditMotherLink() 
 	{
 		return editMotherLink;
 	}
-	//Correct Mother Name field
-	@FindBy ( id = "txtCorrectMother_Name")
-	public WebElement correctMotherNameTextBox;
-	//Correct Mother ID field
-	@FindBy ( id = "txtCorrectMother_ID")
-	public WebElement correctMotherIDTextBox;
-
-	//Confirm button
-	@FindBy ( id = "cmdOK")
-	public WebElement confirmButton;
-	//Exit button
-	@FindBy ( id = "cmdCancel")
-	public WebElement exitButton;
-	
-	
 
 }
