ee360a83add5162876793926064d71750bd0d5bf
sc049886 <sc049886@WIN7DX64-03367.northamerica.cerner.net>
2017-04-27 15:39:10 +0530

    Restored changes

--- src/com/pageObjects/BabyMatchManagePatientInformationPage.java
+++ src/com/pageObjects/BabyMatchManagePatientInformationPage.java
@@ -10,23 +10,29 @@ public class BabyMatchManagePatientInformationPage extends IEDriver
 	
 	
 	//Link to update mother details
-	@FindBy ( id = "editMother")			public WebElement editMotherLink;
-	
-	//Correct Mother Name field
-	@FindBy ( id = "txtCorrectMother_Name")	public WebElement correctMotherNameTextBox;
-	
-	//Correct Mother ID field
-	@FindBy ( id = "txtCorrectMother_ID")	public WebElement correctMotherIDTextBox;
-
-	//Confirm button
-	@FindBy ( id = "cmdOK")					public WebElement confirmButton;
-	
-	//Exit button
-	@FindBy ( id = "cmdCancel")				public WebElement exitButton;
-	
+	@FindBy ( id = "editMother")			
+	public WebElement editMotherLink;
 	public WebElement getEditMotherLink() 
 	{
 		return editMotherLink;
 	}
+	
+	//Correct Mother Name field
+	@FindBy ( id = "txtCorrectMother_Name")	
+	public WebElement correctMotherNameTextBox;
+	
+	//Correct Mother ID field
+	@FindBy ( id = "txtCorrectMother_ID")	
+	public WebElement correctMotherIDTextBox;
+
+	//Confirm button
+	@FindBy ( id = "cmdOK")					
+	public WebElement confirmButton;
+	
+	//Exit button
+	@FindBy ( id = "cmdCancel")				
+	public WebElement exitButton;
+	
+	
 
 }
