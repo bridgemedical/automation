09e948198407201ce7fe2c8895179ce97f3c2a69
ah045577 <ah045577@WIN7DX64-02644.northamerica.cerner.net>
2017-05-18 17:08:09 +0530

    .

--- src/com/bridge/utilities/IEDriver.java
+++ src/com/bridge/utilities/IEDriver.java
@@ -1,51 +1,7 @@
 package com.bridge.utilities;
 
-import java.util.List;
-import java.util.Set;
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.Test;
-
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-
-public class IEDriver {
-
-	public static WebDriver ieDriver;
-	ExtentReports extent;
-	ExtentTest test;
-	String URL;
-	
-	/*
-	 * public void callDriver(String URL) throws InterruptedException{
-	 * 
-	 * System.setProperty("webdriver.ie.driver",
-	 * "C:/IEDriverServer_Win32_2.53.0/IEDriverServer.exe"); driver = new
-	 * InternetExplorerDriver(); driver.navigate().to(URL); Thread.sleep(1000);
-	 * driver.manage().window().maximize();
-	 * 
-	 * 
-	 * 
-	 * }
-	 */
-/*
-	public IEDriver() {
-
-	}
-package com.bridge.utilities;
-
-import java.util.List;
-import java.util.Set;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.testng.annotations.Test;
-
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 
@@ -115,24 +71,6 @@ public class IEDriver {
 
 		return ieDriver;
 
-	}
-
-}
-
-	public IEDriver(String URL) {
-
-		this.URL = URL;
-		ieDriver = iEDriver();
-
-	}
-
-	public WebDriver iEDriver() {
-
-		System.setProperty("webdriver.ie.driver", "C:/IEDriverServer_Win32_2.53.0/IEDriverServer.exe");
-		ieDriver = new InternetExplorerDriver();
-		ieDriver.navigate().to(URL);
-		ieDriver.manage().window().maximize();
-		return ieDriver;
 	}
 
 }
--- src/com/bridge/utilities/SeleniumFunctions.java
+++ src/com/bridge/utilities/SeleniumFunctions.java
@@ -7,15 +7,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumFunctions {
 	Select sel;
-<<<<<<< HEAD
-	IEDriver iedriver = new IEDriver();
-	WebDriverWait wait;
-	//Explicit wait function
-    public void waitForElement(String locator)
-    { 
-          wait = new WebDriverWait(iedriver.ieDriver, 10000);
-    	  wait.until(ExpectedConditions.presenceOfElementLocated(By.id(locator)));
-=======
 	// IEDriver iedriver = new IEDriver();
 	// Explicit wait function
 
@@ -23,33 +14,16 @@ public class SeleniumFunctions {
 	public void selectValueByVisibleText(WebElement element, String visibleText) {
 		sel = new Select(element);
 		sel.selectByVisibleText(visibleText);
->>>>>>> branch 'master' of https://github.com/bridgemedical/automation.git
 	}
-<<<<<<< HEAD
-    
-    public void wait(WebElement locator)
-    { 
-          wait = new WebDriverWait(iedriver.ieDriver, 10000);
-    	  wait.until(ExpectedConditions.visibilityOf(locator));
-	}
-  
-    //Function to select values from drop down.
-	public void selectValueByVisibleText(WebElement element,String visibleText)
-	{
-		 sel = new Select(element);
-	     sel.selectByVisibleText(visibleText);
-=======
 
 	public void selectValueByValueAttribute(WebElement element, String value) {
 		// element = administerpage.dropdownBloodPlasmaDerivative();
 		sel = new Select(element);
 		sel.selectByValue(value);
->>>>>>> branch 'master' of https://github.com/bridgemedical/automation.git
 	}
 
 	public void waitForElement(WebElement locator) {
 		WebDriverWait wait = new WebDriverWait(IEDriver.ieDriver, 10000);
 		wait.until(ExpectedConditions.visibilityOf(locator));
-	}
-
+	} 
 }
